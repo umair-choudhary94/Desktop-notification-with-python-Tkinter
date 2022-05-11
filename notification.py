2c43e58ff4a553fea1c43ebd496e4640bdfe422a
Umair Choudhary <82431716+umair-choudhary94@users.noreply.github.com>
2022-05-11 11:11:32 +0500

    Add files via upload

---
+++ notification.py
@@ -0,0 +1,19 @@
+import time
+
+from plyer import notification
+import time
+
+if __name__ == '__main__':
+ 	while (True):
+		
+
+ 	    notification.notify(
+ 		    title = "**TAKE REST",
+ 		    message ="DRINK WATER\nfocus on your study.\nSee time now.\nyou have to become software engineer.\never waste your time..",
+ 		    app_icon = "D:\project drink\Google-Noto-Emoji-Smileys-10012-smiling-face-with-sunglasses.ico",
+ 	        timeout= 10
+			
+ 	    )
+ 	    
+ 	    time.sleep(60*30)
+		
